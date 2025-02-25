af6ce34d07a731a2f7a76c03c62e6a8c1ca4c27d
Roberto <oliveirarobertw@gmail.com>
2025-02-25 17:37:30 -0300

    returning an HTML

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello, World! <br> PAGE 01')
+    return render(request, 'hello.html')
 
 # Create your views here.
